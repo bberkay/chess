7c2fd2a979ee8eabeda028c515541b659bdcfacd
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2023-03-18 04:04:49 +0300

    filter-playable-squares

--- src/js/Core/Engine.js
+++ src/js/Core/Engine.js
@@ -288,68 +288,6 @@ class Engine {
 
 
     /**
-     * Get playable path to not to be check/avoid endangering the king
-     * @param {int} square_id Square ID of current piece
-     * @param {JSON} playable_squares Playable Squares of Target Piece
-     * @example {"top":[4,6,7], "top-right":[3,2,1]}
-     * @returns {JSON} {"top":[2,3,4], "bottom":[5,6,7]}
-     */
-    #filterPlayableSquares(square_id, playable_squares) {
-        let playable_paths = [];
-        let king_square_id = GameController.getPlayerKingSquareID();
-        let current_piece = GameController.getPieceBySquareID(square_id);
-        
-        
-        for (let i in playable_squares) {
-            let target_square_for_enemy_control = playable_squares[i].slice(-1)[0]; // Get last square of the path
-
-            // Control, is last square has enemy
-            let enemy_control = GameController.isSquareHasPiece(target_square_for_enemy_control, gl_current_move == "white" ? "black" : "white", current_piece.type == "queen" ? ["queen", "bishop", "rook"] : [current_piece.type]);
-            if (enemy_control) {
-                let enemy = GameController.getPieceBySquareID(target_square_for_enemy_control);
-                if (enemy.type == "bishop" || enemy.type == "queen") {
-                    // If enemy type is bishop or queen then current piece just move ...
-                    if (i == "top-left" || i == "bottom-right") {
-                        // if enemy on top-left or bottom-right then current piece just move bottom-right and top-left squares
-                        let target_square_for_king_control = playable_squares[i == "top-left" ? "bottom-right" : "top-left"].slice(-1)[0];
-
-                        // Is current piece guard king
-                        if (i == "top-left" && target_square_for_king_control + 9 == king_square_id || i == "bottom-right" && target_square_for_king_control - 9 == king_square_id)
-                            playable_paths = ["bottom-right", "top-left"];
-
-                    } else if (i == "top-right" || i == "bottom-left") {
-                        // if enemy on top-right or bottom-left then just move bottom-left and top-right squares
-                        let target_square_for_king_control = playable_squares[i == "top-right" ? "bottom-left" : "top-right"].slice(-1)[0];
-
-                        // Is current piece guard king
-                        if (i == "top-right" && target_square_for_king_control + 7 == king_square_id || i == "bottom-left" && target_square_for_king_control - 7 == king_square_id)
-                            playable_paths = ["top-right", "bottom-left"];
-                    }
-                }
-                if (enemy.type == "rook" || enemy.type == "queen") {
-                    // If enemy type is rook or queen then current piece just move ...
-                    if (i == "top" || i == "bottom") {
-                        // If enemy on top or bottom then current piece just move bottom and top squares
-                        let target_square_for_king_control = playable_squares[i == "top" ? "bottom" : "top"].slice(-1)[0];
-
-                        // Is current piece guard king
-                        if (i == "top" && target_square_for_king_control + 8 === king_square_id || i == "bottom" && target_square_for_king_control - 8 == king_square_id)
-                            playable_paths = ["top", "bottom"];
-                    }
-                }
-            }
-        }
-
-        // Delete unplayable paths in playable squares/paths to avoid endangering the king
-        for (let i in playable_squares) {
-            if (!playable_paths.includes(i))
-                delete playable_squares[i];
-        }
-
-        return playable_squares;
-    }
-
-    /**
      * @private
      * Calculate Column of Square
      * @param {int} square_id Square ID of the active piece
@@ -660,4 +598,95 @@ class Engine {
 
         return dangerous_squares.length !== 0;
     }
+
+
+    /**
+     * Get playable path to not to be check/avoid endangering the king
+     * @param {int} square_id Square ID of current piece
+     * @param {JSON} playable_squares Playable Squares of Target Piece
+     * @example {"top":[4,6,7], "top-right":[3,2,1]}
+     * @returns {JSON} {"top":[2,3,4], "bottom":[5,6,7]}
+     */
+    #filterPlayableSquares(square_id, playable_squares){
+        let piece = GameController.getPieceBySquareID(square_id);
+
+        // Is piece can guard king and can kill enemy
+        if(["queen", "rook", "bishop"].includes(piece.type))
+            this.#filterPlayableSquaresCanKill(square_id, playable_squares);
+        else // Is piece just can guard king and not kill enemy(pawn and king)
+            this.#filterPlayableSquaresCantKill(square_id, playable_squares);
+    }
+    
+    
+    /**
+     * Get playable path to not to be check/avoid endangering the king
+     * @param {int} square_id Square ID of current piece
+     * @param {JSON} playable_squares Playable Squares of Target Piece
+     * @example {"top":[4,6,7], "top-right":[3,2,1]}
+     * @returns {JSON} {"top":[2,3,4], "bottom":[5,6,7]}
+     */
+    #filterPlayableSquaresCanKill(square_id, playable_squares) {
+        let playable_paths = [];
+        let king_square_id = GameController.getPlayerKingSquareID();
+        let current_piece = GameController.getPieceBySquareID(square_id);
+        
+
+        for (let i in playable_squares) {
+            let target_square_for_enemy_control = playable_squares[i].slice(-1)[0]; // Get last square of the path
+
+            // Control, is last square has enemy
+            let enemy_control = GameController.isSquareHasPiece(target_square_for_enemy_control, gl_current_move == "white" ? "black" : "white", current_piece.type == "queen" ? ["queen", "bishop", "rook"] : [current_piece.type]);
+            if (enemy_control) {
+                let enemy = GameController.getPieceBySquareID(target_square_for_enemy_control);
+                if (enemy.type == "bishop" || enemy.type == "queen") {
+                    // If enemy type is bishop or queen then current piece just move ...
+                    if (i == "top-left" || i == "bottom-right") {
+                        // if enemy on top-left or bottom-right then current piece just move bottom-right and top-left squares
+                        let target_square_for_king_control = playable_squares[i == "top-left" ? "bottom-right" : "top-left"].slice(-1)[0];
+
+                        // Is current piece guard king
+                        if (i == "top-left" && target_square_for_king_control + 9 == king_square_id || i == "bottom-right" && target_square_for_king_control - 9 == king_square_id)
+                            playable_paths = ["bottom-right", "top-left"];
+
+                    } else if (i == "top-right" || i == "bottom-left") {
+                        // if enemy on top-right or bottom-left then just move bottom-left and top-right squares
+                        let target_square_for_king_control = playable_squares[i == "top-right" ? "bottom-left" : "top-right"].slice(-1)[0];
+
+                        // Is current piece guard king
+                        if (i == "top-right" && target_square_for_king_control + 7 == king_square_id || i == "bottom-left" && target_square_for_king_control - 7 == king_square_id)
+                            playable_paths = ["top-right", "bottom-left"];
+                    }
+                }
+                if (enemy.type == "rook" || enemy.type == "queen") {
+                    // If enemy type is rook or queen then current piece just move ...
+                    if (i == "top" || i == "bottom") {
+                        // If enemy on top or bottom then current piece just move bottom and top squares
+                        let target_square_for_king_control = playable_squares[i == "top" ? "bottom" : "top"].slice(-1)[0];
+
+                        // Is current piece guard king
+                        if (i == "top" && target_square_for_king_control + 8 === king_square_id || i == "bottom" && target_square_for_king_control - 8 == king_square_id)
+                            playable_paths = ["top", "bottom"];
+                    }
+                }
+            }
+        }
+
+        // Delete unplayable paths in playable squares/paths to avoid endangering the king
+        for (let i in playable_squares) {
+            if (!playable_paths.includes(i))
+                delete playable_squares[i];
+        }
+
+        return playable_squares;
+    }
+
+    
+    /**
+     * Get playable path to not to be check/avoid endangering the king
+     * @param {int} square_id Square ID of current piece
+     * @param {JSON} playable_squares Playable Squares of Target Piece
+     * @example {"top":[4,6,7], "top-right":[3,2,1]}
+     * @returns {JSON} {"top":[2,3,4], "bottom":[5,6,7]}
+     */
+    #filterPlayableSquaresCantKill(square_id, playable_squares){}
 }
