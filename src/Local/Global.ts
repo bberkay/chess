b5e893b53df444f744ec130dbc0c67330988ceba
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2023-07-20 04:59:09 +0300

    Global-type-hinting-fixed-and-cache-ability-added

--- src/Local/Global.ts
+++ src/Local/Global.ts
@@ -1,15 +1,24 @@
-class Session{
-    // FIXME: Cache.Set
+import { Cache } from "./Cache";
+import "../Enums";
 
+export class Global {
     /**
      * This class stores the current status of the current game
      */
 
     /**
      * Board of the current game
-     * @example {1: Piece, 2: null, 3: Piece, etc...}
      */
-    private static _squares:Record<Square, Piece|null> = {};
+    private static _squares:Record<Square, Piece|null> = {
+        [Square.a1]: null, [Square.a2]: null, [Square.a3]: null, [Square.a4]: null, [Square.a5]: null, [Square.a6]: null, [Square.a7]: null, [Square.a8]: null,
+        [Square.b1]: null, [Square.b2]: null, [Square.b3]: null, [Square.b4]: null, [Square.b5]: null, [Square.b6]: null, [Square.b7]: null, [Square.b8]: null,
+        [Square.c1]: null, [Square.c2]: null, [Square.c3]: null, [Square.c4]: null, [Square.c5]: null, [Square.c6]: null, [Square.c7]: null, [Square.c8]: null,
+        [Square.d1]: null, [Square.d2]: null, [Square.d3]: null, [Square.d4]: null, [Square.d5]: null, [Square.d6]: null, [Square.d7]: null, [Square.d8]: null,
+        [Square.e1]: null, [Square.e2]: null, [Square.e3]: null, [Square.e4]: null, [Square.e5]: null, [Square.e6]: null, [Square.e7]: null, [Square.e8]: null,
+        [Square.f1]: null, [Square.f2]: null, [Square.f3]: null, [Square.f4]: null, [Square.f5]: null, [Square.f6]: null, [Square.f7]: null, [Square.f8]: null,
+        [Square.g1]: null, [Square.g2]: null, [Square.g3]: null, [Square.g4]: null, [Square.g5]: null, [Square.g6]: null, [Square.g7]: null, [Square.g8]: null,
+        [Square.h1]: null, [Square.h2]: null, [Square.h3]: null, [Square.h4]: null, [Square.h5]: null, [Square.h6]: null, [Square.h7]: null, [Square.h8]: null,
+    };
 
     /**
      * General information about the current game
@@ -95,6 +104,7 @@ class Session{
     }
 
     /**
+     * // FIXME. Bundan kurtulunmaya çalışılacak
      * Get ID list of pieces
      */
     static getIdList(): Array<number>
@@ -104,6 +114,7 @@ class Session{
 
     /**
      * Get castling status
+     * @example Global.getCastlingStatus(CastlingType.WhiteLong)
      */
     static getCastlingStatus(castlingType:CastlingType): boolean
     {
@@ -112,6 +123,7 @@ class Session{
 
     /**
      * Get en passant status of piece
+     * @example Global.getEnPassantStatus(pieceId)
      */
     static getEnPassantStatus(pieceId: number): EnPassantDirection|true
     {
@@ -120,6 +132,7 @@ class Session{
 
     /**
      * Is en passant disabled?
+     * @example Global.isEnPassantDisabled(pieceId, EnPassantDirection.Left) // if result is truer than that means piece(id) can't en passant to left
      */
     static isEnPassantDisabled(pieceId: number, direction: EnPassantDirection|true): EnPassantDirection|boolean
     {
@@ -133,6 +146,9 @@ class Session{
     static setSquare(squareId: Square, content: Piece|null): void
     {
         this._squares[squareId] = content;
+
+        // Add to cache
+        Cache.set(CacheLayer.Game, "squares", this._squares);
     }
 
     /**
@@ -141,6 +157,9 @@ class Session{
     static setNextMove(): void
     {
         this._currentPlayer = this.getEnemyColor();
+
+        // Add to cache
+        Cache.set(CacheLayer.Game, "currentPlayer", this._currentPlayer);
     }
 
     /**
@@ -149,6 +168,9 @@ class Session{
     static increaseMoveCount(): void
     {
         this._moveCount += 1;
+
+        // Add to cache
+        Cache.set(CacheLayer.Game, "moveCount", this._moveCount);
     }
 
     /**
@@ -157,6 +179,9 @@ class Session{
     static setCurrentPlayer(color: Color): void
     {
         this._currentPlayer = color;
+
+        // Add to cache
+        Cache.set(CacheLayer.Game, "currentPlayer", this._currentPlayer);
     }
 
     /**
@@ -165,6 +190,9 @@ class Session{
     static setMoveCount(count: number): void
     {
         this._moveCount = count;
+
+        // Add to cache
+        Cache.set(CacheLayer.Game, "moveCount", this._moveCount);
     }
 
     /**
@@ -177,18 +205,26 @@ class Session{
 
     /**
      * Change castling status
+     * @example Global.changeCastlingStatus(CastlingType.WhiteLong, false) // That means white long castling is disabled
      */
     static changeCastlingStatus(castlingType: CastlingType, value: boolean): void
     {
         this._castlingStatus[castlingType] = value;
+
+        // Add to cache
+        Cache.set(CacheLayer.Game, "castlingStatus", this._castlingStatus);
     }
 
     /**
      * Add piece(id) that can't en passant to en passant status list
+     * @example Global.setDisabledEnPassant(pieceId, EnPassantDirection.Left) // That means piece(id) can't en passant to left
      */
     static setDisabledEnPassant(pieceId: number, direction: EnPassantDirection|true): void
     {
         this._enPassantStatus[pieceId] = direction;
+
+        // Add to cache
+        Cache.set(CacheLayer.Game, "enPassantStatus", this._enPassantStatus);
     }
 
     /**
@@ -197,6 +233,9 @@ class Session{
     static setCheckedPlayer(color: Color|null): void
     {
         this._checkedPlayer = color;
+
+        // Add to cache
+        Cache.set(CacheLayer.Game, "checkedPlayer", this._checkedPlayer);
     }
 
     /**
@@ -207,7 +246,13 @@ class Session{
         this.setMoveCount(0);
         this.setCurrentPlayer(Color.White);
         this.setCheckedPlayer(null);
+
+        // En passant and castling status reset
         this._enPassantStatus = {};
+
+        // Add to cache
+        Cache.set(CacheLayer.Game, "enPassantStatus", this._enPassantStatus);
+
         this._castlingStatus = {
             [CastlingType.WhiteLong]: true,
             [CastlingType.WhiteShort]: true,
@@ -216,6 +261,9 @@ class Session{
             [CastlingType.Long]: true,
             [CastlingType.Short]: true,
         }
+
+        // Add to cache
+        Cache.set(CacheLayer.Game, "castlingStatus", this._castlingStatus);
     }
 
 }
