7ba146691e91ab66a21640dcd6be3fcf2e6f74e0
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2024-10-20 23:42:54 +0300

    missing-comments-added-to-server

--- server/src/Consts/index.ts
+++ server/src/Consts/index.ts
@@ -1,4 +1,6 @@
-// Server settings
+/**
+ * Server Settings
+ */
 export const ALLOWED_ORIGINS = [
     'http://localhost:5173',
 ];
@@ -14,7 +16,9 @@ export const CORS_HEADERS: Record<string, string> = {
 export const SERVER_PORT = 3000;
 export const MAX_PAYLOAD_LENGTH = 1024 * 25; // 25 KB
 
-// Game limits
+/**
+ * Game Settings
+ */
 export const MAX_PLAYER_NAME_LENGTH = 25;
 export const MIN_PLAYER_NAME_LENGTH = 3;
 
@@ -24,5 +28,7 @@ export const MIN_TOTAL_TIME = 15 * 1000; // 15 second
 export const MAX_INCREMENT_TIME = 30 * 1000; // 30 seconds
 export const MIN_INCREMENT_TIME = 0;
 
-// Lobby settings
-export const ID_LENGTH = 6;
+/**
+ * General Settings
+ */
+export const GU_ID_LENGTH = 6; // General Use ID Length
--- server/src/Types/index.ts
+++ server/src/Types/index.ts
@@ -10,6 +10,9 @@ export interface Player{
     token: string;
 }
 
+/**
+ * Parameters required to create a new lobby.
+ */
 export interface CreateLobbyReqParams{
     name: string;
     board: string;
@@ -17,19 +20,37 @@ export interface CreateLobbyReqParams{
     increment: string;
 } 
 
+/**
+ * Parameters required to join an existing lobby.
+ */
 export interface JoinLobbyReqParams{
     name: string;
     lobbyId: string;
 }
 
+/**
+ * Parameters required to reconnect to an existing lobby.
+ */
 export interface ReconnectLobbyReqParams{
     lobbyId: string;
     token: string;
 }
 
+/**
+ * Base interface that combines all WebSocket request parameter types.
+ * This can be used when a type needs to potentially include all properties from the various request types.
+ */
 export interface BaseWebSocketReqParams extends CreateLobbyReqParams, JoinLobbyReqParams, ReconnectLobbyReqParams {}
+
+/**
+ * Union type representing all possible WebSocket request parameter types.
+ * This can be used when a function or method can accept any one of these request types.
+ */
 export type WebSocketReqParams = CreateLobbyReqParams | JoinLobbyReqParams | ReconnectLobbyReqParams;
 
+/**
+ * Represents the data associated with a WebSocket connection.
+ */
 export interface WebSocketData{
     lobbyId: string;
     player: Player;
@@ -38,4 +59,8 @@ export interface WebSocketData{
     incrementTime: string;
 }
 
+/**
+ * Custom type for a ServerWebSocket with WebSocketData.
+ * This combines the Bun ServerWebSocket type with our custom WebSocketData interface.
+ */
 export type RWebSocket = ServerWebSocket<WebSocketData>;
