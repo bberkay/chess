f4bfcfbda6182dd8e8186e55ddda9708d0f93e21
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2024-11-06 01:40:50 +0300

    stockfish-built-path-fixed

--- client/src/Chess/Bot/index.ts
+++ client/src/Chess/Bot/index.ts
@@ -83,7 +83,9 @@ export class Bot {
         WebAssembly.validate(
             Uint8Array.of(0x0, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00)
         );
-    public readonly stockfishPath: string = `Stockfish/${
+    public readonly stockfishPath: string = `${
+        import.meta.env.MODE === "development" ? "Stockfish" : "js/stockfish"
+    }/${
         this.isWasmSupported ? "stockfish.wasm.js" : "stockfish.js"
     }`;
 
