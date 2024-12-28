1304541435c6484beb9f474a77bcbc5d8f5286eb
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2024-12-28 06:05:35 +0300

    Update index.ts

--- client/src/Consts/index.ts
+++ client/src/Consts/index.ts
@@ -12,6 +12,7 @@ export const SERVER_ADDRESS = import.meta.env.NODE_ENV === "development"
 export const WS_ADDRESS = import.meta.env.NODE_ENV === "development"
     ? "ws://localhost:3000"
     : "wss://chess-server-3j94.onrender.com";
+export const WS_ENDPOINT_MAX_LENGTH = 1000;
 
 /**
  * When the socket connection is lost, the client will
