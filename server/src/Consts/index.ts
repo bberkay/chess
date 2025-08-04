52d6bdd8eb10a5a9ac7270caab31d141ebd47528
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2025-08-04 15:54:39 +0300

    server main improvements

--- server/src/Consts/index.ts
+++ server/src/Consts/index.ts
@@ -2,11 +2,16 @@
  * Server Settings
  */
 export const CORS_HEADERS: Record<string, string> = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
-    'Access-Control-Allow-Headers': 'Content-Type',
+    // TODO: Change this
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "OPTIONS, GET, POST",
+    "Access-Control-Allow-Headers": "Content-Type",
     "Cross-Origin-Embedder-Policy": "require-corp",
     "Cross-Origin-Opener-Policy": "same-origin",
+};
+
+export const DEFAULT_CORS_RESPONSE: ResponseInit = {
+    status: 200,
 };
 
 export const SERVER_PORT = 3000;
@@ -19,8 +24,8 @@ export const MAX_IDLE_TIMEOUT = 16 * 60; // 16 minutes
 export const MAX_PLAYER_NAME_LENGTH = 25;
 export const MIN_PLAYER_NAME_LENGTH = 3;
 
-export const MAX_TOTAL_TIME = 60 * 60000; // 60 minutes
-export const MIN_TOTAL_TIME = 15 * 1000; // 15 second
+export const MAX_REMAINING_TIME = 60 * 60000; // 60 minutes
+export const MIN_REMAINING_TIME = 15 * 1000; // 15 second
 
 export const MAX_INCREMENT_TIME = 30 * 1000; // 30 seconds
 export const MIN_INCREMENT_TIME = 0;
@@ -31,4 +36,5 @@ export const DESTROY_INACTIVE_LOBBY_TIMEOUT = 1000 * 60 * 60 * 24; // 24 hours
  * General Settings
  */
 export const GU_ID_LENGTH = 6; // General Use ID Length
-
+export const MIN_FEN_LENGTH = 20;
+export const MAX_FEN_LENGTH = 100;
--- server/src/main.ts
+++ server/src/main.ts
@@ -9,33 +9,4 @@
 
 import { createServer } from "./BunServer";
 
-/**
- * ********* Websocket Connection Rules ***********
- *
- * Creating Lobby:
- * ws://localhost:3000?name=Player1&board=pppppppprnbqkbnr/8/8/8/8/8/PPPPPPPPRNBQKBNR%20w%20KQkq%20-%200%201&totalTime=5&incrementTime=0
- * lobbyId and token will be created randomly by server
- * and sent to client with connected WsCommand. Example:
- * CONNECTED JSON.stringify({
- *  "lobbyId":"123456",
- *  "token":"123456",
- *  "color":"White",
- *  "name":"Player1"
- * })
- *
- * Connection to the lobby:
- * ws://localhost:3000?name=Player1&lobbyId=123456
- * token will be created randomly by server and sent to
- * client with connected WsCommand.
- *
- * Reconneting to the lobby:
- * ws://localhost:3000?token=123456&lobbyId=123456
- * name will be taken from the server by token
- * and sent to client with connected WsCommand.
- * Note: name will be taken from the server
- * by token for preventing the user to reconnect
- * lobby with different name.
- *
- * *************************************************
- */
 createServer();
